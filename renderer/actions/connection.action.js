f22afbb4caa7eed5d46dae38b72afa5f2c45b20f
Elias Júnior <eliasjcjunior@gmail.com>
2019-02-03 15:41:33 -0200

    Remove console.log

--- renderer/actions/connection.action.js
+++ renderer/actions/connection.action.js
@@ -7,7 +7,6 @@ export const startConnection = () => {
     return dispatch => {
         dispatch({ type: actionsType.NEW_CONNECTION , data: { log: 'Connection started'}});
         setTimeout(() => {
-            console.log('aqui');
             dispatch({ type: actionsType.CONNECTION_SUCCESS , data: { log: 'Connection finished'}});
         }, 5000);
       };
